892c87a77143ecfe655651e1753b9f0e65767c4d
WinterCore <hogobbl@gmail.com>
2020-06-12 15:41:25 +0200

    Fixed rubocop

--- src/enumerables.rb
+++ src/enumerables.rb
@@ -1,5 +1,3 @@
-# rubocop: disable Style/CaseEquality, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
-
 module Enumerable
   def my_each
     return enum_for(__method__) unless block_given?
